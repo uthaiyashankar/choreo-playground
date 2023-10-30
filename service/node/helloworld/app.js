7696e4e9058d516f3f30e1c06477e2dc71c37ac4
Selvaratnam Uthaiyashankar <uthaiyashankar@gmail.com>
2023-10-30 18:42:37 +0530

    Hardcode the port on where the service is exposed

--- service/node/helloworld/app.js
+++ service/node/helloworld/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000; //process.env.PORT
 
 app.listen(PORT, () => console.log('Listening on port: ', PORT));
 
